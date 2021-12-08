8bcd733a2a6a76b610370c9b37253ccca96b514a
Nicole Tang <88300660+nicole649@users.noreply.github.com>
2021-12-08 16:57:39 +0800

    version 1

--- chatbot/flask_rowan/app.py
+++ chatbot/flask_rowan/app.py
@@ -3,6 +3,9 @@ from flaskext.mysql import MySQL
 import pymysql
 from pymysql import cursors
 
+import numpy as np
+import random
+import tensorflow as tf
 
 app = Flask(__name__)
 app.secret_key='secret123'
@@ -33,12 +36,53 @@ def explore_page():
         cur = con.cursor()
 
         if request.method == 'GET':
-            cur.execute("SELECT * FROM df_general2 LIMIT 100")
+            cur.execute("SELECT * FROM df_general2")
             itemsDetails = cur.fetchall() 
 
             user = session['userid']
-            cur.execute("SELECT * from wishlist WHERE userid = %s", user)
+            cur.execute("SELECT * from wishlist1 WHERE userid = %s", user)
             wishlistitems = cur.fetchall()
+
+
+            def rec_model(user1):
+                if user1 > 21869:
+                    user = random.sample(range(1,21869,1),1)[0]
+                else:
+                    user = user1
+                
+                cur.execute("select user_loc from df_reviews2 where user_id =  %s", user)
+                user_loc = cur.fetchone()[0]
+                model_re=tf.saved_model.load("re_testing1")
+                model_rank=tf.saved_model.load("rank_testing")
+
+                reviewed_items = []
+                U_reviews = cur.execute("select title from df_reviews2 where user_id =  %s", user1)
+                for i in cur.fetchall():
+                   reviewed_items.append(i[0])
+                U_wishitems = cur.execute("select itemname from wishlist1 where userid =  %s", user1)
+                for i in cur.fetchall():
+                    reviewed_items.append(i[0])
+
+
+                scores, titles = model_re({"user_id":np.array([str(user)]), "user_loc":np.array([str(user_loc)])})
+                retrieval_result = [i.decode('utf-8') for i in titles.numpy()[0]]
+                rank_items = [item for item in retrieval_result if item not in reviewed_items][0:300]
+                rank_ratings = {}
+                for item_title in rank_items[0:20]:
+                    rank_ratings[item_title] = model_rank({"user_id": np.array([str(user)]),"title": np.array([item_title])})
+                Top_rec = []  
+                for title, score in sorted(rank_ratings.items(), key=lambda x: x[1], reverse=True):
+                    Top_rec.append(title)
+                rec_items = Top_rec[0:10]
+                
+                cur.execute(f"select title from df_general2 where category_desc = (select category_desc from df_general2 as a left join wishlist1 as b on a.title = b.itemname where userid = {user1} order by datetime desc limit 1)")
+                sub_cat_list = [i[0] for i in cur.fetchall()]
+                same_cat_items = [item for item in rank_items if item in sub_cat_list and item not in rec_items] 
+                rec_items = rec_items[0:10] + same_cat_items[0:4]
+                rec_items = random.sample(rec_items,10)
+                return rec_items
+            
+            rec_items = rec_model(session['userid'])
 
 
         if request.method == 'POST':
@@ -47,7 +91,7 @@ def explore_page():
             username = itemAdd['username']
             itemid = itemAdd['itemid']
             itemname = itemAdd['itemname']
-            cur.execute("INSERT INTO wishlist(userid, username, itemid, itemname) VALUES (%s, %s, %s, %s)", (userid, username, itemid, itemname))
+            cur.execute("INSERT INTO wishlist1(userid, user_name, itemid, itemname) VALUES (%s, %s, %s, %s)", (userid, username, itemid, itemname))
             con.commit()
             flash('Added successfully!','success')
             return redirect(url_for('explore_page'))
@@ -56,7 +100,22 @@ def explore_page():
         flash('Please log in to view the explore page!','danger')
         return redirect(url_for('login_page'))
 
-    return render_template('explore.html', itemsDetails= itemsDetails, wishlistitems=wishlistitems)
+    return render_template('explore.html', itemsDetails= itemsDetails, wishlistitems=wishlistitems, rec_items=rec_items)
+
+@app.route('/review', methods=['GET','POST'])
+def review_page():
+    if session.get('loggedin'):
+        con = mysql.connect()
+        cur = con.cursor()
+ 
+        cur.execute("SELECT * from df_reviews2 LIMIT 5000")
+        itemReviews= cur.fetchall()
+    
+    else: 
+        flash('Please log in to view the review page!','danger')
+        return redirect(url_for('login_page'))
+    return render_template('review.html', itemReviews= itemReviews)
+
 
 @app.route('/register', methods=['GET','POST'])
 def register_page():
